66d4f3b5f6b230bb0a6b220bd7fad644f34e6f92
Trevon Freeman <trevonfreeman@Trevons-MBP.lan>
2024-04-02 20:43:25 -0700

    Finish play game function and added styling with a little bit of dom

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
+const playerScoreCount = document.querySelector('.playerScores')
+const computerScoreCount = document.querySelector('.computerScores')
+const tiedGameCount = document.querySelector('.tiedGames')
+
+
 const getComputerChoice = () => {
     const computerOption = ['rock','paper','scissors',];
     const randomNumber = Math.floor(Math.random()* computerOption.length);
 
-    console.log(computerOption[randomNumber])
     
     return computerOption[randomNumber];
   }
   
-  const playRound = (playerSelection ,computerSelection) => {    
+  const playRound = (playerSelection ,computerSelection) => { 
     const botName = 'BOT';
     
     let playerChoice = playerSelection.toLowerCase();
@@ -32,64 +36,88 @@ const getComputerChoice = () => {
        console.log(` Computer wins! Try Again`)
      }
     else if(playerChoice === 'paper' && computerSelection === 'rock'){
-       console.log(`${botName} wins! Try Again`)
+      console.log(`${botName} wins! Try Again`);
      }
      else if (!playerChoice === 'rock' || 'paper' || 'scissors'){
-        console.log('enter the correct value')
+       console.log( 'enter the correct value');
      }
     
   }
 
   const playGame = () => {
 const computerChoice = getComputerChoice();
-
-
-
     let playerScore = 0;
     let computerScore = 0;
     let tiedgames= 0;
+    const playerName = prompt('What is your name ');
+
+
     for (let index = 0; index <= 5; index++) {
         const playerSelect = prompt('Rock paper scissors');
-        const playerCase = playerSelect.toLowerCase()
-        playRound(playerCase,computerChoice); 
+        const playerCase = playerSelect.toLowerCase();
 
-        console.log(index)
+        const gameCount = index;
+
+        console.log(index);
+
+
       
         if(playerCase === computerChoice){
-            console.log('Tied game')
-            console.log(++tiedgames)
+            console.log(++tiedgames+ " "+ playerScore + " " + computerScore)
             continue;
         }
         else if(playerCase === 'rock' && computerChoice === 'scissors'){
-          console.log('Rock wins')
           ++playerScore
           console.log(playerScore,computerScore)
     
         }
         else if (playerCase === 'rock' && computerChoice === 'paper'){
-            console.log(` picked Paper you lose`)
             ++computerScore
+          console.log(playerScore,computerScore)
+
           }
          else if (playerCase === 'scissors' && computerChoice === 'rock'){
-          console.log(` Rock wins! Try Again`)
           ++computerScore
+          console.log(playerScore,computerScore)
+
         }
          else if (playerCase === 'scissors' && computerChoice === 'paper'){
-          console.log('Scissors wins! Try Again')
           ++playerScore
+          console.log(playerScore,computerScore)
+
         }
          else if(playerCase === 'paper' && computerChoice=== 'scissors'){
-           console.log(` Computer wins! Try Again`)
            ++computerScore
+          console.log(playerScore,computerScore)
+
          }
         else if(playerCase === 'paper' && computerChoice === 'rock'){
-           console.log(`player wins! Try Again`)
            ++playerScore
+          console.log(playerScore,computerScore);
          }
-         else if (!playerCase === 'rock' || 'paper' || 'scissors'){
-            console.log('enter the correct value')
+        
+         if(index === 5 && playerScore > computerScore){
+            console.log(`Your winner is Tre ${playerName}`);
+            playerScoreCount.textContent = playerScore;
+            computerScoreCount.textContent = computerScore;
+            tiedGameCount.textContent = tiedgames;
             break;
+
          }
+        else if(index === 5 && playerScore < computerScore){
+            console.log('Your winner is computer');
+            playerScoreCount.textContent = playerScore;
+            computerScoreCount.textContent = computerScore;
+            tiedGameCount.textContent = tiedgames;
+            break;
+
+         }
+         
+         
+
+        playRound(playerCase,computerChoice); 
+
+
     }
 
 
@@ -98,9 +126,5 @@ const computerChoice = getComputerChoice();
 
 
   
-// const computerChoice = getComputerChoice()
-// const playerSelect = prompt('Rock paper scissors');
 
-
-// console.log(playRound(playerChoice,computerChoice));
-console.log(playGame())
+console.log(playGame());
